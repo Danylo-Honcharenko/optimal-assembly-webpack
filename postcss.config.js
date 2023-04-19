5e5d0076cd68d98b5c7e05590340bad90061c8f7
Etherd <danilo.gonch@gmail.com>
2023-04-19 15:53:42 +0300

    Optimal Assembly

---
+++ postcss.config.js
@@ -0,0 +1,13 @@
+const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+
+module.exports = {
+    plugins: [
+        tailwindcss,
+        autoprefixer,
+        cssnano({
+            preset: 'default',
+        }),
+    ],
+};
